7b6f852204b86e459d7f27e5fde588e96bf14e73
Paul Lonauer <p.lonauer@gmail.com>
2017-11-14 16:06:37 +0100

    starting the server is now a separate function

--- source/src/TCPMonster.java
+++ source/src/TCPMonster.java
@@ -6,22 +6,22 @@ class TCPMonster {
 	private static final int UPDATE_INTERVAL = 1000;
 	private static final int SERVER_PORT = 40000;
 	
-	// Placeholder data structure; Better divide data into more than one array
-	// Should contain all relevant position data in the game
-	private static float[][][] data = new float[1000][][];
+	private static float[][] data = new float[100][];
 	
-	// Query user input for those two
-	private static int gameId = 1;
-	private static String username = "Shaendro";
+	private static String username = "User";
 
-	public static void main(String argv[]) throws Exception {
+	public static void main(String argv[]) {
 		System.out.println("Are you trying to start a client? (true/false)");
 		Scanner s = new Scanner(System.in);
 		boolean isClient = Boolean.parseBoolean(s.nextLine());
 		s.close();
 		
 		if (isClient) startServerSync();
-		else {
+		else startServer();
+	}
+	
+	public static void startServer() {
+		new Thread(() -> {
 			ServerSocket sSocket = null;
 			try {
 				sSocket = new ServerSocket(SERVER_PORT);
@@ -30,50 +30,10 @@ class TCPMonster {
 					UniversalDTO request = (UniversalDTO) new ObjectInputStream(cSocket.getInputStream()).readObject();
 					if (request.getEvent().equals("Join")) threadedUpdate(cSocket, request);
 				}
-			} finally { sSocket.close(); }
-		}
-	}
-	
-	/**
-	 * Notifies the server about an event that happened to the player
-	 * @param event A {@link String} that describes the event
-	 * @return The {@link UniversalDTO} returned by the server
-	 */
-	public static UniversalDTO notifyServer(String event) {
-		Socket clientSocket = null;
-		UniversalDTO response = null;
-		try {
-			clientSocket = new Socket("localhost", SERVER_PORT);
-			new ObjectOutputStream(clientSocket.getOutputStream()).writeObject(new UniversalDTO(gameId, username, event, data[gameId]));
-			response = (UniversalDTO) new ObjectInputStream(clientSocket.getInputStream()).readObject();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try { clientSocket.close(); } catch (IOException e) {}
-			if (response == null) response = new UniversalDTO(0, "Server", "Connection Failure", null);
-		}
-		return response;
-	}
-
-	/**
-	 * Joins a session on the server and continually processes the {@link UniversalDTO}s sent by the server
-	 */
-	public static void startServerSync() {
-		new Thread(() -> {					
-			Socket clientSocket = null;
-			UniversalDTO response = null;
-			try {
-				clientSocket = new Socket("localhost", SERVER_PORT);
-				new ObjectOutputStream(clientSocket.getOutputStream()).writeObject(new UniversalDTO(gameId, username, "Join", null));
-				while (true) {
-					response = (UniversalDTO) new ObjectInputStream(clientSocket.getInputStream()).readObject();				
-					data[gameId] = response.getData();
-					System.out.println(response.getEvent());
-				}
 			} catch (Exception e) {
 				e.printStackTrace();
-			} finally {
-				try { clientSocket.close(); } catch (IOException e) {}
+			} finally { 
+				try { sSocket.close(); } catch (IOException e) {}
 			}
 		}).start();
 	}
@@ -86,42 +46,78 @@ class TCPMonster {
 	public static void threadedUpdate(Socket cSocket, UniversalDTO request) {
 		new Thread(() -> {
 			try {
-				System.out.println(request.getGameId() + "/" + request.getUsername() + ": Joined");
+				System.out.println(request.getUsername() + ": Joined");
 				while (true) {
-					new ObjectOutputStream(cSocket.getOutputStream()).writeObject(new UniversalDTO(request.getGameId(), "Server", "Update", data[request.getGameId()]));
-					System.out.println(request.getGameId() + "/" + request.getUsername() + ": Update");
+					new ObjectOutputStream(cSocket.getOutputStream()).writeObject(new UniversalDTO("Server", "Update", data));
+					System.out.println(request.getUsername() + ": Update");
 					Thread.sleep(UPDATE_INTERVAL);
 				}
 			} catch (SocketException e) {
-				System.out.println(request.getGameId() + "/" + request.getUsername() + ": Left");
+				System.out.println(request.getUsername() + ": Left");
 			} catch (Exception e) { 
 				e.printStackTrace();
 			}
 		}).start();
 	}
+
+	/**
+	 * Joins a session on the server and continually processes the {@link UniversalDTO}s sent by the server
+	 */
+	public static void startServerSync() {
+		new Thread(() -> {					
+			Socket clientSocket = null;
+			UniversalDTO response = null;
+			try {
+				clientSocket = new Socket("localhost", SERVER_PORT);
+				new ObjectOutputStream(clientSocket.getOutputStream()).writeObject(new UniversalDTO(username, "Join", null));
+				while (true) {
+					response = (UniversalDTO) new ObjectInputStream(clientSocket.getInputStream()).readObject();				
+					data = response.getData();
+					System.out.println(response.getEvent());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try { clientSocket.close(); } catch (IOException e) {}
+			}
+		}).start();
+	}
+	
+	/**
+	 * Notifies the server about an event that happened to the player
+	 * @param event A {@link String} that describes the event
+	 * @return The {@link UniversalDTO} returned by the server
+	 */
+	public static UniversalDTO notifyServer(String event) {
+		Socket clientSocket = null;
+		UniversalDTO response = null;
+		try {
+			clientSocket = new Socket("localhost", SERVER_PORT);
+			new ObjectOutputStream(clientSocket.getOutputStream()).writeObject(new UniversalDTO(username, event, data));
+			response = (UniversalDTO) new ObjectInputStream(clientSocket.getInputStream()).readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try { clientSocket.close(); } catch (IOException e) {}
+			if (response == null) response = new UniversalDTO("Server", "Connection Failure", null);
+		}
+		return response;
+	}
 	
 	/**
 	 * Encompasses the data transferred between the TCP server and client
 	 */
-	
 	private static class UniversalDTO implements Serializable {
 		private static final long serialVersionUID = -4143190306637532691L;
-		private int gameId;
 		private String username;
 		private String event;
 		private float[][] data;
 
-		public UniversalDTO(int gameId, String username, String event, float[][] data) {
-			this.gameId = gameId;
+		public UniversalDTO(String username, String event, float[][] data) {
 			this.username = username;
 			this.event = event;
 			this.data = data;
 		}
-		
-		public int getGameId() {
-			return gameId;
-		}
-
 		public String getUsername() {
 			return username;
 		}
